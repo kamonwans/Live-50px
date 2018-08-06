ff91134a2a3bcb86d32b4909448a31f6fecc24a7
kamonwans <kamonwan294@gmail.com>
2018-08-06 17:06:59 +0700

    create packet configuration and set color format AGGB_8888

---
+++ app/src/main/java/com/example/liuyao/lab/configuration/GlideConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.liuyao.lab.configuration;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.GlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
+public class GlideConfiguration implements GlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+    }
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+
+    }
+}
