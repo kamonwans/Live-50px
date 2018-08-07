90d9be956e033c0fe82b25cf7f922fbc2f457619
kamonwans <kamonwan294@gmail.com>
2018-08-07 16:51:42 +0700

    create file up_from_button

--- app/src/main/java/com/example/liuyao/lab/configuration/GlideConfiguration.java
+++ app/src/main/java/com/example/liuyao/lab/configuration/GlideConfiguration.java
@@ -2,24 +2,22 @@ package com.example.liuyao.lab.configuration;
 
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.Registry;
 import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.GlideModule;
-import com.bumptech.glide.request.RequestOptions;
+
 
 public class GlideConfiguration implements GlideModule {
 
+
     @Override
-    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
-        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+    public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
     }
 
     @Override
-    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+    public void registerComponents(Context context, Glide glide) {
 
     }
 }
